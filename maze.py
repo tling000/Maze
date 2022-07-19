edd01f95fa83288ea7e672960a658682430bae83
tling <tling21490@gmail.com>
2022-07-19 17:15:07 +0900

    探索中の様子をターミナル上に描画

--- maze.py
+++ maze.py
@@ -1,52 +1,86 @@
 #迷路探索(幅優先)
-#迷路を探索し、スタートからゴールへ到達可能か判断する。また最短のターン数を出力する。
+
+#迷路(外壁無し)を探索し、スタートからゴールへ到達可能か判断する。
+#最短のターン数を出力する。
+#探索中の様子を描画する。
+
+from time import sleep
+
+def Figure(depth):
+  #描画
+  sleep(1)
+  figure = str(depth) + "ターン目\n"
+  for y in range(len(Maze)):
+    for x in range(len(Maze[0])):
+      if y==start[0] and x==start[1]:
+        figure += "S "
+      elif Maze[y][x]==9:
+        figure += "G "
+      elif Maze[y][x]==0:
+        figure += "  "
+      elif Maze[y][x]==1:
+        figure += "■"
+      elif Maze[y][x]==2:
+        figure += "* "
+    figure += "\n"
+
+  print(figure)
+
+  return
 
 
-#二次元の迷路を定義(0: 通路、1: 壁)
-Maze = [[0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 1, 1, 1],
-        [0, 1, 1, 0]]
+def TryMaze(start):
+  #探索
+  ans = "No"
+  Position = [start]
+  while len(Position)!=0:
 
-#スタートを設定(y, x, depth)
-start = [4, 0, 0]
-#ゴールを設定(9: ゴール)
-Maze[1][3] = 9
+    #探索するポジション
+    y, x, depth = Position.pop(0)
 
-#迷路を表示
-for i in range(len(Maze)):
-  print(Maze[i])
+    #ゴールに到達したら終了
+    if Maze[y][x]==9:
+      ans = "Yes"
+      break
+    else:
+      Maze[y][x] = 2
+    
+    #次の候補を選択
+    #上
+    if y-1>=0 and Maze[y-1][x]!=1 and Maze[y-1][x]!=2:
+      Position.append([y-1, x, depth+1])
+    #下
+    if y+1<len(Maze) and Maze[y+1][x]!=1 and Maze[y+1][x]!=2:
+      Position.append([y+1, x, depth+1])
+    #右
+    if x+1<len(Maze[0]) and Maze[y][x+1]!=1 and Maze[y][x+1]!=2:
+      Position.append([y, x+1, depth+1])
+    #左
+    if x-1>=0 and Maze[y][x-1]!=1 and Maze[y][x-1]!=2:
+      Position.append([y, x-1, depth+1])
 
-#探索
-ans = "No"
-Position = [start]
-while len(Position)!=0:
-
-  #探索するポジション
-  y, x, depth = Position.pop(0)
-
-  #ゴールに到達したら終了
-  if Maze[y][x]==9:
-    ans = "Yes"
-    break
-  else:
-    Maze[y][x] = 2
+    Figure(depth)
   
-  #次の候補を選択
-  #上
-  if y-1>=0 and Maze[y-1][x]!=1 and Maze[y-1][x]!=2:
-    Position.append([y-1, x, depth+1])
-  #下
-  if y+1<len(Maze) and Maze[y+1][x]!=1 and Maze[y+1][x]!=2:
-    Position.append([y+1, x, depth+1])
-  #右
-  if x+1<len(Maze[0]) and Maze[y][x+1]!=1 and Maze[y][x+1]!=2:
-    Position.append([y, x+1, depth+1])
-  #左
-  if x-1>=0 and Maze[y][x-1]!=1 and Maze[y][x-1]!=2:
-    Position.append([y, x-1, depth+1])
-  #print(Position)
+  return ans, depth
 
-print("到達可否: ", ans)
-print("ターン: ",  str(depth))
+
+
+
+if __name__ == '__main__':
+
+  #二次元の迷路を定義(0: 通路、1: 壁)
+  Maze = [[0, 0, 0, 0],
+          [0, 1, 1, 0],
+          [0, 0, 0, 0],
+          [0, 1, 1, 1],
+          [0, 1, 1, 0]]
+
+  #スタートを設定(y, x, depth)
+  start = [4, 0, 0]
+  #ゴールを設定(9: ゴール)
+  Maze[1][3] = 9
+
+  #試行、結果を表示
+  ans, depth = TryMaze(start)
+  print("到達可否: ", ans)
+  print("ターン: ",  depth)
