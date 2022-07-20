373bbf46d6674e5a6d98fcbda957142902c1a599
tling <tling21490@gmail.com>
2022-07-20 11:04:38 +0900

    マップを更新、png形式で画像保存するよう修正

--- depth_first_search.py
+++ depth_first_search.py
@@ -4,10 +4,13 @@
 #ターン数を出力する。
 #探索中の様子を描画する。
 
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 from time import sleep
 
 def Figure(depth):
-  #描画
+  #ターミナル上に描画
   sleep(1)
   figure = str(depth) + "ターン目\n"
   for y in range(len(Maze)):
@@ -25,6 +28,15 @@ def Figure(depth):
     figure += "\n"
 
   print(figure)
+
+  #png形式で保存
+  basename = "figure"
+  if not os.path.exists(basename):
+    os.mkdir(basename)
+  plt.figure()
+  sns.heatmap(Maze, cbar=False, square=True)
+  plt.savefig(basename + "/U%02d.png" % depth)
+  plt.close()
 
   return
 
@@ -74,16 +86,21 @@ def TryMaze(start):
 if __name__ == '__main__':
 
   #二次元の迷路を定義(0: 通路、1: 壁)
-  Maze = [[0, 0, 1, 0],
-          [0, 1, 1, 0],
-          [0, 0, 0, 0],
-          [0, 1, 1, 1],
-          [0, 1, 1, 0]]
+  Maze = [[0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+          [0, 1, 1, 0, 1, 0, 1, 0, 0, 1],
+          [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+          [0, 1, 1, 1, 0, 1, 0, 0, 0, 0],
+          [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+          [0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+          [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+          [0, 1, 1, 1, 0, 0, 1, 1, 1, 0],
+          [0, 1, 1, 0, 0, 1, 0, 0, 1, 0],
+          [0, 1, 1, 1, 1, 0, 1, 1, 1, 0]]
 
   #スタートを設定(y, x, depth)
-  start = [4, 0, 0]
+  start = [0, 0, 0]
   #ゴールを設定(9: ゴール)
-  Maze[1][3] = 9
+  Maze[6][6] = 9
 
   #試行、結果を表示
   TryMaze(start)
